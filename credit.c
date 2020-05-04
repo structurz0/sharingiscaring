a4ae6d7df7f6885e038e4acfee0aa817e5f3d85d
structurz0 <structurz0@users.noreply.github.com>
2020-05-04 00:28:08 +0000

    automated commit by submit50 [lang=C.UTF-8]

--- credit.c
+++ credit.c
@@ -15,17 +15,22 @@ int main(void)
     long long credit_card_number;
     do
 
-    //Ask for credit card number
+//Ask for credit card number
     {
         credit_card_number = get_long_long("Number: ");
-    } while (credit_card_number < 0);
-
+    }
+    while (credit_card_number < 0);
     if (check_validity(credit_card_number) == true)
+    {
         print_credit_card_brand(credit_card_number);
-    else
+    }
+    else 
+    {
         printf("INVALID\n");
+    }
 }
 
+// Check if it is valid
 bool check_validity(long long credit_card_number)
 {
     int len = find_length(credit_card_number);
@@ -33,6 +38,7 @@ bool check_validity(long long credit_card_number)
     return (len == 13 || len == 15 || len == 16) && checksum(credit_card_number);
 }
 
+// Check the length of the credit card to determine
 int find_length(long long n)
 {
     int len;
@@ -46,7 +52,9 @@ bool checksum(long long ccn)
     for (int i = 0; ccn != 0; i++, ccn /= 10)
     {
         if (i % 2 == 0)
+        {
             sum += ccn % 10;
+        }
         else
         {
             int digit = 2 * (ccn % 10);
@@ -56,15 +64,31 @@ bool checksum(long long ccn)
     return (sum % 10) == 0;
 }
 
+//Print the name of the credit card processor
 void print_credit_card_brand(long long ccn)
 {
-    if ( (ccn >= 34e13 && ccn < 35e13) || (ccn>= 37e13 && ccn < 38e13) )
-        printf("AMEX\n");
-    else if (ccn >= 51e14 && ccn < 56e14)
-        printf("MASTERCARD\n");
-    else if ((ccn >= 4e12 && ccn < 5e12) || (ccn >= 4e15 && ccn < 5e15))
-        printf("VISA\n");
-    else
-        printf("INVALID\n");
+    if ((ccn >= 34e13 && ccn < 35e13) || (ccn >= 37e13 && ccn < 38e13))
 
+    {
+        printf("AMEX\n");
+    }
+    else if (ccn >= 51e14 && ccn < 56e14)
+
+    {
+        printf("MASTERCARD\n");
+    }
+
+    else if ((ccn >= 4e12 && ccn < 5e12) || (ccn >= 4e15 && ccn < 5e15))
+
+    {
+        printf("VISA\n");
+    }
+
+    else
+    { 
+        printf("INVALID\n");
+    }
 }
+
+
+
